5886c1b7d42e14bf2a686ac8162d4c9463753807
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-03-06 21:41:39 +0100

    Get token via http head and return on failure
    Get the error code via http head request and return with exitcode 1 if there was an error or no csrf token was provided

--- fhemcl.sh
+++ fhemcl.sh
@@ -32,8 +32,11 @@ else
     hosturl=$1
 fi
 
-# get Token 
-token=$(curl -s -D - "$hosturl/fhem?XHR=1" | awk '/X-FHEM-csrfToken/{print $2}')
+# get Token via http Head request
+token=$(curl -Is "$hosturl/fhem?XHR=1" | awk '/X-FHEM-csrfToken/{print $2}')
+if [ -z "${token}" ]; then 
+	exit 1
+fi
 
 # reading FHEM command, from Pipe, File or Arguments 
 # Check to see if a pipe exists on stdin.
